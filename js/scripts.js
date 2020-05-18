0aac0287f2e687a60093738d1148658131ab9e04
abdirahman <27870abdirahmanabdidict19s@gmail.com>
2020-05-18 10:53:18 +0300

    added script.js business logic decision on prompt customer where delivery will be made and alert the charges for delivery and where location to be deliver if an option for customer is delivery

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,9 @@ function Pizza(size,crust,top) {
   this.crust = crust;
   this.top = top;
 });
+if (delivery){
+  prompt('Enter where your delivery will be made');
+  console.log(alert("delivery will be deliver to your location and delivery charge is $10"));
+} else
+console.log("thank you for being in contact with us welcome again");
+});
